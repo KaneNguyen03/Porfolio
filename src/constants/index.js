8716139b7f69d385cad9ba877ea2b185eddb4ecf
XuanKha <nguyenxuankhka5371@GMAIL.COM>
2023-03-21 10:57:56 +0700

    fix content

--- src/constants/index.js
+++ src/constants/index.js
@@ -51,14 +51,14 @@ const services = [
     title: "Web Developer",
     icon: web,
   },
-  {
-    title: "React Native Developer",
-    icon: mobile,
-  },
-  {
-    title: "Backend Developer",
-    icon: backend,
-  },
+  // {
+  //   title: "React Native Developer",
+  //   icon: mobile,
+  // },
+  // {
+  //   title: "Backend Developer",
+  //   icon: backend,
+  // },
   {
     title: "Content Creator",
     icon: creator,
